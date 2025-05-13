3ade1e20caceafdb0290b22bd06bd334ca54d886
Raycoms <ray.neiheiser@gmail.com>
2025-05-13 20:47:06 +0200

    Builder unstuck (#10845)
    Builder now always properly requests all the buckets even in the edge cases. (Should speed up builder)
    Builder can now recover from positions becoming unavailable mid build (before the builder would insist until teleporting there)
    Builder now continues placing in a bit of a range around the last position while pathing to the next position.

--- src/main/java/com/minecolonies/core/colony/buildings/modules/BuildingResourcesModule.java
+++ src/main/java/com/minecolonies/core/colony/buildings/modules/BuildingResourcesModule.java
@@ -176,8 +176,9 @@ public class BuildingResourcesModule extends AbstractBuildingModule implements I
     @Nullable
     public BuilderBucket getRequiredResources()
     {
-        return (buckets.isEmpty() || ((AbstractBuildingStructureBuilder) building).getProgress() == null
-                  || ((AbstractBuildingStructureBuilder) building).getProgress().getB() == BuildingStructureHandler.Stage.CLEAR) ? null : buckets.getFirst();
+        return (buckets.isEmpty()
+            || ((AbstractBuildingStructureBuilder) building).getProgress() == null
+            || ((AbstractBuildingStructureBuilder) building).getProgress().getB() == BuildingStructureHandler.Stage.CLEAR) ? null : buckets.getFirst();
     }
 
     /**
@@ -368,7 +369,7 @@ public class BuildingResourcesModule extends AbstractBuildingModule implements I
                 }
             }
 
-            worker.createRequestAsync(new Stack(itemStack.getItemStack(), requestCount * ((AbstractBuildingStructureBuilder) building).getResourceBatchMultiplier(), 1));
+            building.createRequest(new Stack(itemStack.getItemStack(), requestCount * ((AbstractBuildingStructureBuilder) building).getResourceBatchMultiplier(), 1), true);
         }
     }
 
